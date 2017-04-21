d56fdfcdeb8170f2a6487203678c21179f09790d
Jared <jareda.prince@ymail.com>
2017-04-21 08:40:55 -0500

    Update KDC.c

--- KDC.c
+++ KDC.c
@@ -1,1 +1,36 @@
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int KA = 70;
+int KB = 221;
+
+void *ClientA(){
+
+}
+
+void *ClientB(){
+    
+}
+
+void *KDC(){
+
+}
+
+int main(int argc, char *argv[]){
+        pthread_attr_t attr; /* set of thread attributes */
+        pthread_t tidA; /* the thread identifier */
+        pthread_t tidB; /* the thread identifier */
+        pthread_t tidKDC; /* the thread identifier */
+
+        /* get default attribute */
+        pthread_attr_init(&attr);
+
+        /*create the intitial thread */
+    	pthread_create(&tidKDC, &attr, KDC, NULL);
+
+        /*create the client threads */
+    	pthread_create(&tidA, &attr, ClientA, NULL);
+        pthread_create(&tidB, &attr, ClientB, NULL);
+}
