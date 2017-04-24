af71d45e177a0683d84e3d6d32f65d060c391f04
Jared <jareda.prince@ymail.com>
2017-04-24 11:35:06 -0500

    Update KDC.c

--- KDC.c
+++ KDC.c
@@ -18,18 +18,18 @@ struct msg {
 void *ClientA(void* params){
     printf("wefwefwf2");
     //open the mailbox
-    int msqid = msgget(16777217, 0600 | IPC_CREAT);
+    int msqidA = msgget(16777217, 0600 | IPC_CREAT);
+    int msqidKDC = msgget(16777216, 0600 | IPC_CREAT);
+    int msqidB = msgget(16777218, 0600 | IPC_CREAT);
     struct msg msgp;
 
     msgp.mtype = 0;
     msgp.mdata[0] = 0;
     msgp.mdata[1] = 3;
 
-    printf("wefwefwf");
+    msgsnd(msqidKDC, &msgp, sizeof(msgp)-sizeof(long), 0);
 
-    msgsnd(msqid, &msgp, sizeof(msgp)-sizeof(long), 0);
-
-    msgrcv(msqid, &msgp, sizeof(msgp)-sizeof(long), 0, 0);
+    msgrcv(msqidB, &msgp, sizeof(msgp)-sizeof(long), 0, IPC_NOWAIT);
 
     pthread_exit(0);
 }
@@ -44,30 +44,31 @@ void *ClientB(void* params){
 void *KDC(void* params){
     printf("wefwefwf3");
     int msqid = msgget(16777216, 0600 | IPC_CREAT);
+    int msqidA = msgget(16777217, 0600 | IPC_CREAT);
     struct msg msgp;
 
     printf("wefwefwf4");
 
-    while(running == 1){
-        //wait for message from the first process
-        msgrcv(msqid, &msgp, sizeof(msgp)-sizeof(long), 0, 0);
+    // while(running == 1){
+    //     //wait for message from the first process
+    //     msgrcv(msqid, &msgp, sizeof(msgp)-sizeof(long), 0, 0);
 
-        //generate a random session key
-        int sessionKey = rand();
+    //     //generate a random session key
+    //     int sessionKey = rand();
         
-        //get the shared keys fo the two processes
-        int shKeyA = sharedKeys[msgp.mdata[0]];
-        int shKeyB = sharedKeys[msgp.mdata[1]];
+    //     //get the shared keys fo the two processes
+    //     int shKeyA = sharedKeys[msgp.mdata[0]];
+    //     int shKeyB = sharedKeys[msgp.mdata[1]];
 
-        printf("%d", shKeyA);
-        printf("%d", shKeyB);
+    //     printf("%d", shKeyA);
+    //     printf("%d", shKeyB);
 
-        //encrypt the session keys with the shared keys
-        int eKeyA = sessionKey ^ shKeyA;
-        int eKeyB = sessionKey ^ shKeyB;
+    //     //encrypt the session keys with the shared keys
+    //     int eKeyA = sessionKey ^ shKeyA;
+    //     int eKeyB = sessionKey ^ shKeyB;
 
-        msgsnd(msqid, &msgp, sizeof(msgp)-sizeof(long), 0);
-    }
+    //     msgsnd(msqid, &msgp, sizeof(msgp)-sizeof(long), 0);
+    // }
 
     pthread_exit(0);
 }
@@ -83,12 +84,12 @@ int main(int argc, char *argv[]){
 
         /*create the intitial thread */
     	// pthread_create(&tidKDC, &attr, KDC, NULL);
-        
+
         /*create the client threads */
     	pthread_create(&tidA, &attr, ClientA, NULL);
         pthread_create(&tidB, &attr, ClientB, NULL);
 
-        pthread_join(tidKDC, NULL);
+        // pthread_join(tidKDC, NULL);
         pthread_join(tidA, NULL);
         pthread_join(tidB, NULL);
 }
